8075d36906d3b931bcb82227ddf72556712ab28d
yuumairie <rujiangyoumo42@gmail.com>
2021-01-30 00:26:27 +0900

    layout共通化

--- app/controllers/route_main.go
+++ app/controllers/route_main.go
@@ -1,15 +1,9 @@
 package controllers
 
 import (
-	"log"
 	"net/http"
-	"text/template"
 )
 
 func top(w http.ResponseWriter, r *http.Request) {
-	t, err := template.ParseFiles("app/views/templates/top.html")
-	if err != nil {
-		log.Fatalln(err)
-	}
-	t.Execute(w, "hello")
+	generateHTML(w, "Hello", "layout", "top")
 }
--- app/controllers/server.go
+++ app/controllers/server.go
@@ -1,9 +1,21 @@
 package controllers
 
 import (
+	"fmt"
 	"net/http"
+	"text/template"
 	"todo_app/config"
 )
+
+func generateHTML(w http.ResponseWriter, data interface{}, filenames ...string) {
+	var files []string
+	for _, file := range filenames {
+		files = append(files, fmt.Sprintf("app/views/templates/%s.html", file))
+	}
+
+	templates := template.Must(template.ParseFiles(files...))
+	templates.ExecuteTemplate(w, "layout", data)
+}
 
 //StartMainServer サーバ立ち上げ
 func StartMainServer() error {
